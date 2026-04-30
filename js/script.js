823b731bbab03ee526941b127ff59f8495321e63
rohanad1561 <adhikaryrohan76@gmail.com>
2026-04-30 13:00:19 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -27,6 +27,45 @@ let splide = new Splide( '#image-carousel', {
 
 splide.mount();
 
+ let splideb = new Splide('#card-carousel', {
+    type: 'loop',
+    perPage: 4,
+    perMove: 1,
+    gap: '20px',
+    breakpoints: {
+      768: {
+        perPage: 2,
+        gap: '0.7rem',
+      },
+      576: {
+        perPage: 1,
+        gap: '0.7rem',
+      },
+    },
+  });
+
+  splideb.mount();
+
+ let splidet = new Splide('#t-card-slider', {
+    type: 'loop',
+    perPage: 1,
+    perMove: 1,
+    gap: '20px',
+    breakpoints: {
+      768: {
+        perPage: 1,
+        gap: '0.7rem',
+      },
+      576: {
+        perPage: 1,
+        gap: '0.7rem',
+      },
+    },
+  });
+
+  splidet.mount();
+
+
 let splidea = new Splide( '#auto', {
   type   : 'loop',
   drag   : 'free',
